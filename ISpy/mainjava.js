aa744610788479a387184107d932b582f5391105
pommelpro <pommelpro@gmail.com>
2014-04-10 23:00:25 -0500

    11pm code

--- ISpy/mainjava.js
+++ ISpy/mainjava.js
@@ -1,88 +1,80 @@
-var yeses = 0;
-var count = 0;
-
 var buswait = new Array();
+var buswait11 = new Array();
+var buswait12 = new Array();
+var buswait13 = new Array();
+var buswait21 = new Array();
+var buswait22 = new Array();
+var buswait23 = new Array();
+var buswait31 = new Array();
+var buswait32 = new Array();
+var buswait33 = new Array();
+
+
 
 buswait[0] = "img/busbench.png";
 buswait[1] = "img/businessphone.jpg";
 buswait[2] = "img/tree.png";
 buswait[3] = "img/baby.jpeg";
 
-var titles = new Array();
 
-titles[0] = "Bench";
-titles[1] = "Phone Call";
-titles[2] = "Tree";
-titles[3] = "Baby";
-
-
-var which = Math.floor((Math.random()*buswait.length));
-
+var which11 = Math.floor((Math.random()*buswait11.length));
+var which12 = Math.floor((Math.random()*buswait12.length));
+var which13 = Math.floor((Math.random()*buswait13.length));
+var which21 = Math.floor((Math.random()*buswait21.length));
+var which22 = Math.floor((Math.random()*buswait22.length));
+var which23 = Math.floor((Math.random()*buswait23.length));
+var which31 = Math.floor((Math.random()*buswait31.length));
+var which32 = Math.floor((Math.random()*buswait32.length));
+var which33 = Math.floor((Math.random()*buswait33.length));
 
 $(document).ready(
 	function()
 	{
-	 $('body').hide().fadeIn(1000);
-
-	 $('img').attr('src',buswait[which]);
-	 $('#texter').text(titles[which]);
-	 $('#responses').html("Found: " + yeses);
+	for(var i = 0; i < buswait.length; i++) {
+			buswait11[i] = buswait[i];
 	}
-);
-
-
-
-function yesans()
-{
-	if(buswait.length != 1)
-	{
-		yeses++;
-		buswait.splice(which,1);
-		titles.splice(which,1);
-		which = Math.floor((Math.random()*buswait.length));
-		$('img').attr('src',buswait[which]);
-		$('#texter').text(titles[which]);
-		$('#responses').html("Found: " + yeses);
+	for(var i = 0; i < buswait.length; i++) {
+			buswait12[i] = buswait[i];
 	}
-	else
-	{
-		if(count != 1)
-		{
-			yeses++;
-			count = 1;
-			$('#responses').html("Found: " + yeses);
-		}
-		$('img').attr('src',"");
-		$('#texter').text("");
-	}
-}
+	$('body').hide().fadeIn(1000);
+	$('#piconeone').attr('src', buswait11[which11]);
+	$('#piconetwo').attr('src', buswait12[which12]);
+/*	$('#piconethree').attr('src', buswait13[which13]);
+	$('#pictwoone').attr('src', buswait21[which21]);
+	$('#pictwotwo').attr('src', buswait22[which22]);
+	$('#pictwothree').attr('src', buswait23[which23]);
+	$('#picthreeone').attr('src', buswait31[which31]);
+	$('#picthreetwo').attr('src', buswait32[which32]);
+	$('#picthreethree').attr('src', buswait33[which33]);
+*/
+	$( "#piconeone" ).click(function()
+	{;
+		$('#responses').html(buswait11);
+			if(buswait11.length != 1)
+			{
+				buswait11.splice(which11,1);
+				which11 = Math.floor((Math.random()*buswait11.length));
+				$('#piconeone').attr('src',buswait11[which11]);
+				$('#responses').html(buswait);
+			}
+			else
+			{
+				$('#piconeone').attr('src',"");
+				$('#responses').html(buswait11);
+			}
+	});
+	$( "#piconetwo" ).click(function()
+	{;
+			if(buswait12.length != 1)
+			{
+				buswait12.splice(which12,1);
+				which12 = Math.floor((Math.random()*buswait12.length));
+				$('#piconetwo').attr('src',buswait12[which12]);
 
-function noans()
-{
-	if(buswait.length != 0)
-	{
-		if(buswait.length > 1)
-		{
-			buswait.splice(which,1);
-			titles.splice(which,1);
-			lengtharr = buswait.length;
-			which = Math.floor((Math.random()*buswait.length));
-			$('img').attr('src',buswait[which]);
-			$('#texter').text(titles[which]);
-		}
-		else
-		{
-			$('img').attr('src',"");
-			$('#texter').text("");
-		}
-		$('#responses').html("Found: " + yeses);
-	}
-}
-
-
-
-
-
-
-
-
+			}
+			else
+			{
+				$('#piconetwo').attr('src',"");
+			}
+	});
+});
